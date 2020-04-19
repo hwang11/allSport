3e20955c4d4af70761d0a860539eaa639a81c16a
황선아 <tjsdk2769@gmail.com>
2020-04-19 14:04:56 +0900

    fix lombok annotation in contest.java

--- src/main/java/com/teamSupport/allSport/dto/Contest.java
+++ src/main/java/com/teamSupport/allSport/dto/Contest.java
@@ -5,27 +5,14 @@ import org.springframework.data.annotation.Id;
 
 import lombok.*;
 
-@Data
 @Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-
 public class Contest {
 	@Id
 	private int idContest;
-	private int contest_num; // 
 	private String contest_startDate; //date로 형식 바꾸기  
 	private String contest_endDate; //date 
-	private String contest_SOCName; //주최기관 
-	private String contest_kind; //종목 
-	private String contest_title; //제목 
-	private String contest_contents; // 내용 
-	private String contest_country; //국내/국제 
-	private String contest_target; //대상 
-	private String contest_place; //장소 
+	private String contest_title; //제목
 	private String contest_URL;
-	private String contest_phone;
-	
-	
+	private String contest_local;
 }
